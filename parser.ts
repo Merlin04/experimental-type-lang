5eb56bfe51f42d714b971ddad42d5656ba848671
merlin04 <Merlin04@users.noreply.github.com>
2021-09-10 21:11:42 -0700

    Add new parser

--- parser.ts
+++ parser.ts
@@ -1,146 +1,132 @@
-import { ast, Expression, _ExtendsExpression } from "./index";
+import { ast, evalAst } from "./index";
+import grammar from "./g.ohm-recipe";
 import fs from "fs";
 
 export default function parse(input: string): ast {
-    const items = input.split(";");
-    // Each item should be an item in the ast array
-    return items.map(item => item.trim()).map(item => {
-        // Determine if it's a TypeDeclaration or an Expression
-        if(item.startsWith("type ")) {
+    const semantics = grammar.createSemantics();
+    semantics.addOperation("toAST", {
+        Program(astItems) {
+            return astItems.children.map(child => child.toAST())[0].filter((item: any) => item !== "");
+        },
+        AstItem(item) {
+            return item.toAST();
+        },
+        TypeDeclaration(_, name, _2, parameters, _3, _4, definition) {
             return {
                 __typename: "TypeDeclaration",
-                name: item.split(" ")[1],
-                parameters: item.split("<")[1].split(">")[0].split(",").map(param => param.trim()).map(param => ({
-                    name: param.split("=")[0].trim(),
-                    defaultValue: parseExpression(param.split("=")[1]?.trim())
-                })),
-                definition: parseExpression(item.split("=")[1].trim())
-            }
-        }
-        else {
-            return parseExpression(item.trim());
-        }
-    });
-}
-
-function parseExpression(input: string): Expression {
-    // Types of expressions:
-    // CallExpression, NumberLiteralExpression, ItemLiteralExpression, ArrayLiteralExpression, ConditionExpression, AbortLiteralExpression, ParameterReferenceExpression
-
-    // Check for CallExpression
-    if(input.split("<").length > 1 && !input.split("<")[0].trim().includes(" ")) {
-        return {
-            __typename: "CallExpression",
-            callee: input.split("<")[0].trim(),
-            parameters: input.split("<")[1].split(">")[0].split(",").map(param => param.trim()).map(param => parseExpression(param))
-        }
-    }
-
-    // Check for NumberLiteralExpression
-    if(!isNaN(Number(input))) {
-        return {
-            __typename: "NumberLiteralExpression",
-            value: Number(input)
-        }
-    }
-
-    // Check for ItemLiteralExpression
-    if(input === "_") {
-        return {
-            __typename: "ItemLiteralExpression"
-        }
-    }
-
-    // Check for ArrayLiteralExpression
-    if(input.startsWith("[") && input.endsWith("]")) {
-        return {
-            __typename: "ArrayLiteralExpression",
-            items: input.substring(1, input.length - 1).split(",").map(element => element.trim()).map(element => {
-                // parseExpression can't normally handle SpreadExpressions, so we need to check for them here
-                if(element.startsWith("...")) {
-                    return {
-                        __typename: "SpreadExpression",
-                        value: parseExpression(element.substring(3))
-                    }
-                }
-                
-                return parseExpression(element);
-            })
-        }
-    }
-
-    // Check for ConditionExpression (__ extends __ ? __ : __)
-    if(input.split(" extends ").length > 1) {
-        return {
-            __typename: "ConditionExpression",
-            evaluatee: parseExpression(input.split(" extends ")[0].trim()),
-            condition: parseConditionExpressionCondition(input.split(" extends ")[1].split(" ? ")[0].trim()),
-            true: parseExpression(input.split(" extends ")[1].split(" ? ")[1].split(" : ")[0].trim()),
-            false: parseExpression(input.split(" extends ")[1].split(" ? ")[1].split(" : ")[1].trim())
-        }
-    }
-
-    // Check for AbortLiteralExpression
-    if(input === "abort") {
-        return {
-            __typename: "AbortLiteralExpression"
-        }
-    }
-
-    // Check for ParameterReferenceExpression
-    if(!input.includes(" ")) {
-        return {
-            __typename: "ParameterReferenceExpression",
-            name: input
-        }
-    }
-
-    throw new Error("Encountered unknown token");
-}
-
-function parseConditionExpressionCondition(input: string): Expression | _ExtendsExpression {
-    // We can tell an ArrayLiteralExpression from an ExtendsExpression by if the source contains "infer"
-    // Technically it would work if we just used ExtendsExpressions but that would decrease performance
-
-    // Check if it looks like an array
-    if(input.startsWith("[") && input.endsWith("]")) {
-        // Split by commas and check if anything starts with "infer" or "...infer"
-        const arrayElements = input.substring(1, input.length - 1).split(",").map(element => element.trim());
-        if(arrayElements.some(element => element.startsWith("infer") || element.startsWith("...infer"))) {
+                name: name.toAST(),
+                parameters: parameters.toAST(),
+                definition: definition.toAST()
+            };
+        },
+        TypeParameterDeclaration_defaultValue(name, _, defaultValue) {
+            return {
+                name: name.toAST(),
+                defaultValue: defaultValue.toAST()
+            };
+        },
+        TypeParameterDeclaration(name) {
+            const evaled = name.toAST();
+            return typeof evaled === "string" ? { name: evaled } : evaled;
+        },
+        ParensExpression(_, expression, _2) {
+            return expression.toAST();
+        },
+        CallExpression(callee, _, parameters, _2) {
+            return {
+                __typename: "CallExpression",
+                callee: callee.toAST(),
+                parameters: parameters.toAST()
+            };
+        },
+        ItemLiteralExpression(_) {
+            return {
+                __typename: "ItemLiteralExpression"
+            };
+        },
+        ArrayLiteralExpression(_, items, _2) {
+            return {
+                __typename: "ArrayLiteralExpression",
+                items: items.toAST()
+            };
+        },
+        SpreadExpression(_, expression) {
+            return {
+                __typename: "SpreadExpression",
+                value: expression.toAST()
+            };
+        },
+        ConditionExpression(evaluatee, _, condition, _2, consequent, _3, alternate) {
+            return {
+                __typename: "ConditionExpression",
+                evaluatee: evaluatee.toAST(),
+                condition: condition.toAST(),
+                true: consequent.toAST(),
+                false: alternate.toAST()
+            };
+        },
+        ExtendsExpression(_, items, _2) {
             return {
                 __typename: "ExtendsExpression",
-                items: arrayElements.map(element => {
-                    if(element.startsWith("infer")) {
-                        return {
-                            __typename: "InferExpression",
-                            name: element.substring(5).trim(),
-                            spread: false
-                        };
-                    }
-                    else if(element.startsWith("...infer")) {
-                        return {
-                            __typename: "InferExpression",
-                            name: element.substring(8).trim(),
-                            spread: true
-                        };
-                    }
-
-                    return parseExpression(element);
-                })
-            }
+                items: items.toAST()
+            };
+        },
+        InferExpression(prefix, expression) {
+            return {
+                __typename: "InferExpression",
+                spread: prefix.source.contents === "...infer ",
+                name: expression.toAST()
+            };
+        },
+        AbortLiteralExpression(_) {
+            return {
+                __typename: "AbortLiteralExpression"
+            };
+        },
+        ParameterReferenceExpression(name) {
+            return {
+                __typename: "ParameterReferenceExpression",
+                name: name.toAST()
+            };
+        },
+        number(digits) {
+            return {
+                __typename: "NumberLiteralExpression",
+                value: Number(digits.sourceString)
+            };
+        },
+        identifier(node) {
+            return node.sourceString;
+        },
+        // Things provided by Ohm but not defined in the grammar
+        NonemptyListOf(item, _, rest) {
+            return [item.toAST(), ...rest.toAST()];
+        },
+        EmptyListOf() {
+            return [];
+        },
+        _iter(children) {
+            return children.map(c => c.toAST());
+        },
+        _terminal() {
+            return this.sourceString;
         }
-        else {
-            return parseExpression(input);
-        }
+    });
+    const match = grammar.match(input);
+    if (match.succeeded()) {
+        return semantics(match).toAST();
+    } else {
+        throw new Error(match.message);
     }
-
-    // Otherwise, it's just a normal expression
-    return parseExpression(input);
 }
 
-// Read the exampleProgram.type file and parse it
-function parseExampleProgram(): ast {
+function parseExampleProgram() {
     return parse(fs.readFileSync("./exampleProgram.type", "utf8"));
 }
 
-console.log(parseExampleProgram());
+// Read the exampleProgram.type and parse/run it
+function runExampleProgram() {
+    return evalAst(parse(fs.readFileSync("./exampleProgram.type", "utf8")));
+}
+
+console.log(JSON.stringify(runExampleProgram(), null, 4));
